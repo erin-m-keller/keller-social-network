405a63d960d4224b1035b06e85bb76b8a9608e17
Erin Keller <aestheticartist@gmail.com>
2023-06-07 13:39:17 -0400

    added user controller, with get and create functions included

---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+  // Get all courses
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+};
